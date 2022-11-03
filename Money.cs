c01da18cf68941bb45066b394cf09b86730a1e75
DuradazhiVlad <dvlad1995p@gmail.com>
2022-11-03 19:47:19 +0200

    first commit

---
+++ Money.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Moneyy
+{
+    public class Money
+    {
+        public double money;
+        public void Show() {
+            if (money >= 0) Console.WriteLine($"money = {money}");
+            else { Console.WriteLine("Bancrot"); }
+        }
+
+        public Money(double money)
+        {
+            this.money = money;
+        }
+        public static Money operator +(Money m, Money m2) { return new Money(m.money + m2.money); }
+        public static Money operator -(Money m, Money m2) { return new Money(m.money - m2.money); }
+        public static Money operator *(Money m, Money m2) { return new Money(m.money * m2.money); }
+        public static Money operator /(Money m, Money m2) { return new Money(m.money + m2.money); }
+        public static Money operator ++(Money m) { return new Money(++m.money); }
+        public static Money operator --(Money m) { return new Money(--m.money); }
+        public static bool operator <(Money m, Money m2) { return m.money < m2.money; }
+        public static bool operator >(Money m, Money m2)
+        {
+             return m.money > m2.money;
+            
+        }
+        public static bool operator ==(Money m, Money m2)
+        {
+             return m.money==m2.money;
+
+        }
+        public static bool operator !=(Money m, Money m2)
+        {
+            return m.money != m2.money;
+
+        }
+
+    } 
+    
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿ using Moneyy;
+
+Money mony = new (1.8);
+Money mony2 = new Money(2.3);
+(mony + mony2).Show();
+
+   
+    
